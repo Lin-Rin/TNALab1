75eb605d82b61b2c45ffbbbc4011a29c5ec86a8c
ssayme <kravchenkoantonn1@gmail.com>
2023-06-02 14:33:36 +0300

    Fixed SimplicityTest and rework power, now it`s modpow.

--- lab1/src/main/SimplicityTest.java
+++ lab1/src/main/SimplicityTest.java
@@ -6,20 +6,36 @@ public class SimplicityTest {
     private final Tools tool = new Tools();
 
     public boolean isPrime(long p) {
-        if (p < 2)
+        if (p % 2 == 0) {
             return false;
-        double s = Math.sqrt(p);
-        for (int i = 2; i <= s; i++) {
-            if (p % i == 0)
+        }
+
+        for (int k = 0; k < 125; k++) {
+            Random random = new Random();
+            long x = random.nextLong(p);
+
+            if (x == 0) {
+                x++;
+            }
+            if (x == p) {
+                x--;
+            }
+
+            if (tool.gcd(p, x) > 1) {
                 return false;
+            }
+
+            long jacobiRes = tool.jacobi(x, p);
+            if (jacobiRes < 0) {
+                jacobiRes = p - 1;
+            }
+
+            // gcd = jacobi = modPower = 1 = proste
+            if (jacobiRes != tool.modPow(x, (p - 1) / 2, p)) {
+                return false;
+            }
         }
         return true;
     }
 
-    public static void main(String[] args) {
-        int i ;
-        for ( i = 0; i < 10; i++) {
-            System.out.println(new SimplicityTest().isPrime(i));
-        }
-    }
 }
--- lab1/src/main/Tools.java
+++ lab1/src/main/Tools.java
@@ -5,58 +5,58 @@ import java.util.ArrayList;
 
 public class Tools {
 
-    private final static String FILE_PATH1  = "lab1/src/res/primes100k.txt";
-    private final static String FILE_PATH2  = "lab1/src/res/primes200k.txt";
-    private final static String FILE_PATH3  = "lab1/src/res/primes300k.txt";
-    private final static String FILE_PATH4  = "lab1/src/res/primes400k.txt";
-    private final static String FILE_PATH5  = "lab1/src/res/primes500k.txt";
-    private final static String FILE_PATH6  = "lab1/src/res/primes600k.txt";
-    private final static String FILE_PATH7  = "lab1/src/res/primes700k.txt";
-    private final static String FILE_PATH8  = "lab1/src/res/primes800k.txt";
-    private final static String FILE_PATH9  = "lab1/src/res/primes900k.txt";
+    private final static String FILE_PATH1 = "lab1/src/res/primes100k.txt";
+    private final static String FILE_PATH2 = "lab1/src/res/primes200k.txt";
+    private final static String FILE_PATH3 = "lab1/src/res/primes300k.txt";
+    private final static String FILE_PATH4 = "lab1/src/res/primes400k.txt";
+    private final static String FILE_PATH5 = "lab1/src/res/primes500k.txt";
+    private final static String FILE_PATH6 = "lab1/src/res/primes600k.txt";
+    private final static String FILE_PATH7 = "lab1/src/res/primes700k.txt";
+    private final static String FILE_PATH8 = "lab1/src/res/primes800k.txt";
+    private final static String FILE_PATH9 = "lab1/src/res/primes900k.txt";
     private final static String FILE_PATH10 = "lab1/src/res/primes1000k.txt";
 
     public long[] getPrimeNumbers() throws IOException {
-        InputStream srcFile1  = new FileInputStream(Tools.FILE_PATH1);
-        InputStream srcFile2  = new FileInputStream(Tools.FILE_PATH2);
-        InputStream srcFile3  = new FileInputStream(Tools.FILE_PATH3);
-        InputStream srcFile4  = new FileInputStream(Tools.FILE_PATH4);
-        InputStream srcFile5  = new FileInputStream(Tools.FILE_PATH5);
-        InputStream srcFile6  = new FileInputStream(Tools.FILE_PATH6);
-        InputStream srcFile7  = new FileInputStream(Tools.FILE_PATH7);
-        InputStream srcFile8  = new FileInputStream(Tools.FILE_PATH8);
-        InputStream srcFile9  = new FileInputStream(Tools.FILE_PATH9);
+        InputStream srcFile1 = new FileInputStream(Tools.FILE_PATH1);
+        InputStream srcFile2 = new FileInputStream(Tools.FILE_PATH2);
+        InputStream srcFile3 = new FileInputStream(Tools.FILE_PATH3);
+        InputStream srcFile4 = new FileInputStream(Tools.FILE_PATH4);
+        InputStream srcFile5 = new FileInputStream(Tools.FILE_PATH5);
+        InputStream srcFile6 = new FileInputStream(Tools.FILE_PATH6);
+        InputStream srcFile7 = new FileInputStream(Tools.FILE_PATH7);
+        InputStream srcFile8 = new FileInputStream(Tools.FILE_PATH8);
+        InputStream srcFile9 = new FileInputStream(Tools.FILE_PATH9);
         InputStream srcFile10 = new FileInputStream(Tools.FILE_PATH10);
         ArrayList<Long> res = new ArrayList<>();
 
-        for(String s : readFromInputStream(srcFile1).split("\n")){
-                res.add(Long.parseLong(s));
-        }
-        for(String s : readFromInputStream(srcFile2).split("\n")){
+        for (String s : readFromInputStream(srcFile1).split("\n")) {
             res.add(Long.parseLong(s));
         }
-        for(String s : readFromInputStream(srcFile3).split("\n")){
+        for (String s : readFromInputStream(srcFile2).split("\n")) {
             res.add(Long.parseLong(s));
         }
-        for(String s : readFromInputStream(srcFile4).split("\n")){
+        for (String s : readFromInputStream(srcFile3).split("\n")) {
             res.add(Long.parseLong(s));
         }
-        for(String s : readFromInputStream(srcFile5).split("\n")){
+        for (String s : readFromInputStream(srcFile4).split("\n")) {
             res.add(Long.parseLong(s));
         }
-        for(String s : readFromInputStream(srcFile6).split("\n")){
+        for (String s : readFromInputStream(srcFile5).split("\n")) {
             res.add(Long.parseLong(s));
         }
-        for(String s : readFromInputStream(srcFile7).split("\n")){
+        for (String s : readFromInputStream(srcFile6).split("\n")) {
             res.add(Long.parseLong(s));
         }
-        for(String s : readFromInputStream(srcFile8).split("\n")){
+        for (String s : readFromInputStream(srcFile7).split("\n")) {
             res.add(Long.parseLong(s));
         }
-        for(String s : readFromInputStream(srcFile9).split("\n")){
+        for (String s : readFromInputStream(srcFile8).split("\n")) {
             res.add(Long.parseLong(s));
         }
-        for(String s : readFromInputStream(srcFile10).split("\n")){
+        for (String s : readFromInputStream(srcFile9).split("\n")) {
+            res.add(Long.parseLong(s));
+        }
+        for (String s : readFromInputStream(srcFile10).split("\n")) {
             res.add(Long.parseLong(s));
         }
 
@@ -117,15 +117,17 @@ public class Tools {
         return a == 1 ? s : s * jacobi(n, a);
     }
 
-    public long power(long a, long n) {
-        long res = 1;
-        while (n > 0) {
-            if ((n & 1) == 1) {
-                res = (res * a) % n;
-            }
-            a = (a * a) % n;
-            n >>= 1;
+    public long modPow(long a, long e, long p) {
+        long result = 1;
+        a %= p;
+
+        while (e > 0) {
+            if (e % 2 == 1)
+                result = (result * a) % p;
+            a = (a * a) % p;
+            e /= 2;
         }
-        return res;
+
+        return result;
     }
 }
